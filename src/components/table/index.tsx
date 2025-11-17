32efc3bb937f83d5c564c837659381a2c245a43e
Danil <posikunets999@gmail.com>
2025-11-18 02:11:48 +0300

    delete bestplayer from table

--- src/components/table/index.tsx
+++ src/components/table/index.tsx
@@ -1,6 +1,5 @@
 import type { TAverage } from "../../models/PlayerStats";
-import { BestPlayer } from "../bestPlayer";
-import { Player } from "../player";
+import { Player } from "../Player";
 import style from "./Table.module.scss";
 
 export function Table({ average }: { average: TAverage[] }) {
@@ -21,9 +20,8 @@ export function Table({ average }: { average: TAverage[] }) {
 
   return (
     <ul className={style.list}>
-      {<BestPlayer stats={sortedPlayers[0]} />}
-      {sortedPlayers.slice(1).map((stats) => (
-        <Player stats={stats} key={stats.id} />
+      {sortedPlayers.map((stats, index) => (
+        <Player key={stats.id} stats={stats} best={index === 0} />
       ))}
     </ul>
   );
